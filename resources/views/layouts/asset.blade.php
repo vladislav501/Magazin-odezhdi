651061efa721e9d873daf3e1bacacd8fed1d0e24
daniil <tarima.949@gmail.com>
2025-02-11 13:05:23 +0300

    подключены стили

--- resources/views/layouts/asset.blade.php
+++ resources/views/layouts/asset.blade.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>P&K</title>
 </head>
 <body>
     <header>
+        <button class="button">11111111111111</button>
         <div class="headerContainer">
             <a href="{{ route('home.index') }}"></a>
             <img src="#" class="#">
